fab290f3ce58d4e4111ea5015377af1e33c38d3e
PeriYumYum <perilin124@gmail.com>
2023-03-25 10:06:45 +0800

    feat: update card and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Flex, FlexProps } from '@chakra-ui/react';
 
 export const Footer = (props: FlexProps) => (
-  <Flex as='footer' direction='column' mt='3rem' py='3rem' {...props} />
+  <Flex as='footer' direction='column' mt='3rem' pt='3rem' {...props} />
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import {
   Button,
   Link,
 } from '@chakra-ui/react';
-import { Heading, SimpleGrid, Center, Stack, Spacer, HStack, VStack, Box } from '@chakra-ui/layout';
+import { Heading, SimpleGrid, Spacer, VStack, Box } from '@chakra-ui/layout';
 import { Hero } from '../components/Hero';
 import { Container } from '../components/Container';
 import { Main } from '../components/Main';
@@ -20,8 +20,8 @@ import { Footer } from '../components/Footer';
 import { GetStaticProps } from 'next';
 import { useState } from 'react';
 
-/*Define object type of playlistItems resource 
-reference: https://developers.google.com/youtube/v3/docs/playlistItems*/
+// /*Define object type of playlistItems resource
+// reference: https://developers.google.com/youtube/v3/docs/playlistItems*/
 type High = {
   high: {
     url: string;
@@ -78,56 +78,54 @@ const Index = ({ data }) => {
       <IframePlayer video_id={currentVideo.snippet.resourceId.videoId} autoPlay={playing} />
       <Main>
         <SimpleGrid columns={[1, 2, 3]} spacingX={10} spacingY={14}>
-          {data.map((video: Data) => {
-            return (
-              <Card
-                key={video.id}
-                borderRadius='xl'
-                bg='#faf9f9'
-                _dark={{
-                  bg: '#242A2B',
-                  color: 'white',
-                }}
-              >
-                <CardBody p='0'>
-                  <AspectRatio maxW='560px' ratio={16 / 9}>
-                    <Image
-                      src={video.snippet.thumbnails.high.url || 'https://via.placeholder.com/300'}
-                      alt={`${video.snippet.title} thumbnail`}
-                      borderTopRadius='xl'
-                    />
-                  </AspectRatio>
-                  <Stack px={4} mt={4} spacing={2}>
-                    <Heading as='h5' fontSize='md'>
-                      {video.snippet.title}
-                    </Heading>
-                    <Text color='text'>{video.snippet.videoOwnerChannelTitle}</Text>
-                  </Stack>
-                </CardBody>
-                <CardFooter p={4} pt='0'>
-                  <Spacer />
-                  <Button
-                    onClick={() => {
-                      setCurrentVideo(video);
-                      scrollToTop();
-                      setPlaying(true);
-                    }}
-                    variant='outline'
-                    colorScheme='teal'
-                    rounded='xl'
-                    size='md'
-                  >
-                    <FaPlay />
-                  </Button>
-                </CardFooter>
-              </Card>
-            );
-          })}
+          {data.map((video: Data) => (
+            <Card
+              key={video.id}
+              borderRadius='xl'
+              bg='#faf9f9'
+              _dark={{
+                bg: '#242A2B',
+                color: 'white',
+              }}
+            >
+              <CardBody p='0'>
+                <AspectRatio maxW='560px' ratio={16 / 9}>
+                  <Image
+                    src={video.snippet.thumbnails.high.url || 'https://via.placeholder.com/300'}
+                    alt={`${video.snippet.title} thumbnail`}
+                    borderTopRadius='xl'
+                  />
+                </AspectRatio>
+                <Box p={4}>
+                  <Heading as='h5' fontSize='md'>
+                    {video.snippet.title}
+                  </Heading>
+                  <Text color='text'>{video.snippet.videoOwnerChannelTitle}</Text>
+                </Box>
+              </CardBody>
+              <CardFooter p={4} pt='0'>
+                <Spacer />
+                <Button
+                  onClick={() => {
+                    setCurrentVideo(video);
+                    scrollToTop();
+                    setPlaying(true);
+                  }}
+                  variant='outline'
+                  colorScheme='teal'
+                  rounded='xl'
+                  size='md'
+                >
+                  <FaPlay />
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
         </SimpleGrid>
       </Main>
       <DarkModeSwitch />
       <Footer>
-        <VStack m={3}>
+        <VStack m={3} textAlign='center'>
           <Text fontSize='lg'>
             Powered and Designed by{' '}
             <Link href='https://github.com/PeriYumYum' isExternal>
@@ -136,15 +134,19 @@ const Index = ({ data }) => {
             ©2023
           </Text>
           <CTA />
+          <Box pos='relative' pt={8} bottom='0'>
+            <Text color='teal'>
+              <Link
+                href='https://www.flaticon.com/free-icons/lesson'
+                title='lesson icons'
+                isExternal
+              >
+                Lesson icons created by Freepik - Flaticon
+              </Link>
+            </Text>
+          </Box>
         </VStack>
       </Footer>
-      <Box pos='relative' bottom='0'>
-        <Text color='teal'>
-          <Link href='https://www.flaticon.com/free-icons/lesson' title='lesson icons' isExternal>
-            Lesson icons created by Freepik - Flaticon
-          </Link>
-        </Text>
-      </Box>
     </Container>
   );
 };
