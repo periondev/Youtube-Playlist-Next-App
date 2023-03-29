fb811921d80567ea429d2f3e19b392ddc48113b2
PeriYumYum <perilin124@gmail.com>
2023-03-29 21:41:31 +0800

    refactor: change Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
+import theme from '../theme';
+import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <>
+      <Head>
+        <title>My Youtube Collection</title>
+        <meta name='discription' content='A YouTube playlist with video player' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
