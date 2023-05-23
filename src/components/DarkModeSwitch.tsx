5af31245df1f19bca49df7f54f8dea3ff8e56a0d
PeriYumYum <perilin124@gmail.com>
2023-05-23 13:53:12 +0800

    Remove Tooltip, add title of button

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -5,23 +5,22 @@ export const DarkModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === 'dark';
   return (
-    <Tooltip label={isDark ? 'Turn light' : 'Turn dark'}>
-      <IconButton
-        position='fixed'
-        top={4}
-        right={4}
-        icon={isDark ? <SunIcon boxSize={5} /> : <MoonIcon boxSize={5} />}
-        aria-label='Toggle Theme'
-        onClick={toggleColorMode}
-        rounded='button'
-        opacity={0.75}
-        bg='#bab1b3'
-        _hover={{ bg: '#948589' }}
-        _dark={{
-          bg: '#353D40',
-          _hover: { bg: '#536063' },
-        }}
-      />
-    </Tooltip>
+    <IconButton
+      title={isDark ? 'Turn light' : 'Turn dark'}
+      position='fixed'
+      top={4}
+      right={4}
+      icon={isDark ? <SunIcon boxSize={5} /> : <MoonIcon boxSize={5} />}
+      aria-label='Toggle Theme'
+      onClick={toggleColorMode}
+      rounded='button'
+      opacity={0.75}
+      bg='#bab1b3'
+      _hover={{ bg: '#948589' }}
+      _dark={{
+        bg: '#353D40',
+        _hover: { bg: '#536063' },
+      }}
+    />
   );
 };
