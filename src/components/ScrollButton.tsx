66ba2ca994d373ae52f247138fad5e47ff4dba49
PeriYumYum <perilin124@gmail.com>
2023-05-24 11:15:33 +0800

    Update scroll button

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -1,26 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import { IconButton } from '@chakra-ui/react';
 import { FaArrowUp } from 'react-icons/fa';
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-};
 
 export const ScrollToTopBtn = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 480) {
+      setShowTopBtn(true);
+    } else {
+      setShowTopBtn(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
-    <IconButton
-      position='fixed'
-      bottom={4}
-      right={4}
-      icon={<FaArrowUp />}
-      aria-label='Scroll to top'
-      onClick={scrollToTop}
-      isRound={true}
-      opacity={0.7}
-      bg='#bab1b3'
-      _hover={{ bg: '#948589' }}
-      _dark={{
-        bg: '#353D40',
-        _hover: { bg: '#536063' },
-      }}
-    />
+    <>
+      {showTopBtn && (
+        <IconButton
+          position='fixed'
+          bottom={4}
+          right={4}
+          icon={<FaArrowUp />}
+          aria-label='Scroll to top'
+          onClick={scrollToTop}
+          isRound={true}
+          opacity={0.7}
+          bg='#bab1b3'
+          _hover={{ bg: '#948589' }}
+          _dark={{
+            bg: '#353D40',
+            _hover: { bg: '#536063' },
+          }}
+        />
+      )}
+    </>
   );
 };
