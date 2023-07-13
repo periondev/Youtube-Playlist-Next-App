c5f5cf3d39e729fa025db42399a5630e09f6bc17
PeriYumYum <perilin124@gmail.com>
2023-07-13 14:41:48 +0800

    Renamed Main.tsx to MainArea.tsx and update stack width

--- src/components/Main.tsx
+++
@@ -1,5 +0,0 @@
-import { Stack, StackProps } from '@chakra-ui/react';
-
-export const Main = (props: StackProps) => (
-  <Stack spacing='1.5rem' width='92%' maxWidth='60rem' p='2rem' px='1rem' {...props} />
-);
---
+++ src/components/MainArea.tsx
@@ -0,0 +1,3 @@
+import { Stack, StackProps } from '@chakra-ui/react';
+
+export const MainArea = (props: StackProps) => <Stack width='88%' maxWidth='60rem' {...props} />;
