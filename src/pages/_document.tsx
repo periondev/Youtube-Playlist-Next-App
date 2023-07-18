fb6d3ad917bc83669e518120eece1f65f2756113
PeriYumYum <perilin124@gmail.com>
2023-07-18 23:22:15 +0800

    Add links and meta tag inside Head to support PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,13 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <meta name='theme-color' content='#fff' />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
